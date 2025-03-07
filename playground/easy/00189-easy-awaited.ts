1aa989a3fae280b67f47766d1e48cf3173206b77
Sedong <bulssajo00@gmail.com>
2025-03-07 14:20:58 +0900

    solve: awaited

--- playground/easy/00189-easy-awaited.ts
+++ playground/easy/00189-easy-awaited.ts
@@ -22,7 +22,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyAwaited<T> = any
+
+type MyAwaited<T> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any> // 재귀적으로 확인
+    ? MyAwaited<U> 
+    : U
+  : never;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
