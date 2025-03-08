506c001b93d4868aa424b255ab274d18fd1fb002
Sedong <bulssajo00@gmail.com>
2025-03-08 19:32:51 +0900

    solve: trim

--- playground/medium/00108-medium-trim.ts
+++ playground/medium/00108-medium-trim.ts
@@ -17,8 +17,8 @@
 */
 
 /* _____________ Your Code Here _____________ */
-
-type Trim<S extends string> = any
+type Space = ' ' | '\n' | '\t'
+type Trim<S extends string> = S extends `${Space}${infer L}` ? Trim<L> : S extends `${infer R}${Space}` ? Trim<R> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
