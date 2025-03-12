863133d1694e78c886614c5fa2c818f5f255ae6c
Sedong <bulssajo00@gmail.com>
2025-03-12 16:20:54 +0900

    solve: merge

--- playground/medium/00599-medium-merge.ts
+++ playground/medium/00599-medium-merge.ts
@@ -27,7 +27,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Merge<F, S> = any
+type Merge<F extends Record<string, any>, S extends Record<string, any>> = {
+  [K in keyof F | keyof S ]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
