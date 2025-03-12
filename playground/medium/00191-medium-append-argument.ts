72364b606ccddacfe3ff2d61f6206f3f99f690e6
Sedong <bulssajo00@gmail.com>
2025-03-12 13:52:14 +0900

    solve: AppendArgument

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -23,7 +23,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends Function, A> = Fn extends (...args: infer R) => infer T ? (...args: [...R, A]) => T : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
