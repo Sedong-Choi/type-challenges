82d47203dee3a12ac5a653ab4232bc627ce0e42b
Sedong <bulssajo00@gmail.com>
2025-03-13 13:27:52 +0900

    solve: remove index signature

--- playground/medium/01367-medium-remove-index-signature.ts
+++ playground/medium/01367-medium-remove-index-signature.ts
@@ -22,9 +22,14 @@
 */
 
 /* _____________ Your Code Here _____________ */
-
-type RemoveIndexSignature<T> = any
-
+type RemoveIndexSignature<T> = {
+  [K in keyof T as string extends K
+    ? never
+    : number extends K ? never
+      : symbol extends K ? never
+        : K
+  ]: T[K]
+}
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
