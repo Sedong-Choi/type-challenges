284e1bfb63682bec3d51359e72df9b3230c3ed7d
Sedong <bulssajo00@gmail.com>
2025-03-13 19:13:36 +0900

    solve: startswith

--- playground/medium/02688-medium-startswith.ts
+++ playground/medium/02688-medium-startswith.ts
@@ -20,7 +20,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type StartsWith<T extends string, U extends string> = any
+type StartsWith<T extends string, U extends string> = U extends '' ? true : T extends `${infer F}${infer R}`
+  ? U extends `${F}${infer UR}` ? StartsWith<R, UR> : false
+  : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
