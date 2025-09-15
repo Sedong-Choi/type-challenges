b4ac1f1db7bb04f285d50127d80b83b1901677d5
Sedong <bulssajo00@gmail.com>
2025-09-16 00:13:36 +0900

    solve: flattendepth

--- playground/medium/03243-medium-flattendepth.ts
+++ playground/medium/03243-medium-flattendepth.ts
@@ -21,7 +21,21 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FlattenDepth = any
+// 한 번 flatten
+type FlattenOnce<T extends any[]> =
+  T extends [infer F, ...infer R]
+    ? F extends any[]
+      ? [...F, ...FlattenOnce<R>]
+      : [F, ...FlattenOnce<R>]
+    : []
+
+// FlattenDepth: 카운터 배열만 사용
+type FlattenDepth<T extends any[], D extends number = 1, C extends any[] = []> =
+  C['length'] extends D
+    ? T
+    : FlattenOnce<T> extends T
+      ? T
+      : FlattenDepth<FlattenOnce<T>, D, [...C, any]>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -34,6 +48,8 @@ type cases = [
   Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
   Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
   Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<FlattenDepth<[[1, [2, [3, [4, [5]]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<FlattenDepth<[[1], [2, [3, [4, [[5]]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
 ]
 
 /* _____________ Further Steps _____________ */
