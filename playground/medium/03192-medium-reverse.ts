15ff169a69f5c4a5fbe874526e2ab1dc8d79876b
Sedong <bulssajo00@gmail.com>
2025-09-15 23:39:23 +0900

    solve: reverse

--- playground/medium/03192-medium-reverse.ts
+++ playground/medium/03192-medium-reverse.ts
@@ -19,7 +19,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Reverse<T> = any
+type Reverse<T extends any[]> = T extends [infer First extends string, ...infer Rest extends string[]] ? [...Reverse<Rest>, First] : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
